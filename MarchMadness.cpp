0981bb86c7343f39bfb9f762ffe55a9b175ce60e
sjhalloran2 <sjhalloran@csbsju.edu>
2019-03-25 20:22:08 +0000

    testing file reading

--- MarchMadness.cpp
+++ MarchMadness.cpp
@@ -1,1 +1,36 @@
 // This program ranks college basketball teams using the outcomes from past games.
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class MarchMadness{
+    public:
+ofstream myfile;
+string line;
+string date;
+int team1, team1Score, team2, team2Score;
+
+MarchMadness(){
+    //myfile.open("ncaaD1.txt", ios::in);
+    ifstream inputFile("marks.txt");
+    cout << "test" << endl;
+    inputFile >> team1;
+    inputFile >> line;
+    cout << date << endl;
+    cout << team1 << endl;
+    while(inputFile >> date >> team1 >> team1Score >> team2 >> team2Score){
+        cout << "test2" << endl;
+        cout << date << team1 << team1Score << team2 << team2Score << endl;
+    }
+
+    myfile.close();
+}
+};
+
+int main(){
+ MarchMadness* test = new MarchMadness();
+ delete test;
+ return 0;
+}
