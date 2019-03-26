4c4d374d4f978615d432ed57d4c41c5fd1b73e05
slaufers001 <slaufers001@csbsju.edu>
2019-03-26 03:04:24 +0000

    Made a method that prints the file with correct format

--- MarchMadness.cpp
+++ MarchMadness.cpp
@@ -6,26 +6,43 @@
 using namespace std;
 
 class MarchMadness{
-    public:
-ofstream myfile;
-string line;
-string date;
-string team1, team1Score, team2, team2Score;
 
-MarchMadness(){
-    //myfile.open("ncaaD1.txt", ios::in);
-    ifstream inputFile("ncaaD1.txt");
-    cout << "test" << endl;
-    while(inputFile >> date >> team1 >> team1Score >> team2 >> team2Score){
-        cout << date << ", " << team1 << ", " << team1Score << ", " << team2 << ", " << team2Score << ", " << endl;
+    // Global Variables
+    public:
+    ofstream outputFile;
+    string line;
+    string date;
+    string team1, team1Score, team2, team2Score;
+
+    // Constructor
+    MarchMadness(){
+        //myfile.open("ncaaD1.txt", ios::in);
+        //ifstream inputFile("ncaaD1.txt");
+        // while(inputFile >> date >> team1 >> team1Score >> team2 >> team2Score){
+        //     cout << date << '\t' << team1 << '\t' << team1Score << '\t' << team2 << '\t' << team2Score << endl;
+        // }
+        printFile();
+        outputFile.close();
     }
 
-    myfile.close();
-}
+    // For testing purposes
+    void printFile(){
+        string line;
+        ifstream inputFile("ncaaD1.txt"); // Creates a file to read from and opens it
+
+        if(inputFile.is_open()){ // Check if the file is open
+            while(getline(inputFile,line)){ // While there is a line of text/strings...
+                cout << line << endl;
+            }
+        }
+
+        inputFile.close(); // Close the file
+    }
 };
 
+// Main
 int main(){
- MarchMadness* test = new MarchMadness();
- delete test;
- return 0;
+    MarchMadness* test = new MarchMadness();
+    delete test;
+    return 0;
 }
